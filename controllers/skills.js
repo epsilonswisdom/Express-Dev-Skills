d75aa01c7f3f87ab0708f476e1a82a5ef72794e0
Manny <emaldonado1402@gmail.com>
2023-01-17 18:51:17 -0500

    Fix error

---
+++ controllers/skills.js
@@ -0,0 +1,65 @@
+import { Skill } from "../models/skill.js"
+
+function index(req, res) {
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills,
+      time: req.time
+    })
+  })
+  .catch(error => { // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function newSkill(req, res) {
+  res.render('skills/new')
+}
+
+function create(req, res) {
+  console.log(req.body)
+  req.body.done = false
+  Skill.create(req.body)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+function show(req, res) {
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+function deleteTodo(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+
+export {
+  index,
+  newSkill as new,
+  create,
+  show,
+  deleteTodo as delete, 
+}
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+import "dotenv/config.js"
+
 import createError from 'http-errors'
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
+import methodOverride from 'method-override'
+//Connects to the database with mongooose
+import './config/database.js'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as skillsRouter } from './routes/skills.js'
 
 // set up app
 const app = express()
@@ -18,6 +23,12 @@ app.set(
 )
 app.set('view engine', 'ejs')
 
+app.use(function(req, res, next) {
+  console.log('Hello SEI!')
+  req.time = new Date().toLocaleTimeString()
+  next()
+})
+
 // middleware
 app.use(logger('dev'))
 app.use(express.json())
@@ -27,10 +38,17 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
+app.use(methodOverride('_method'))
 
 // mounted routers
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/skills', skillsRouter)
+
+app.use(function(req, res, next) {
+  console.log('Hello SEI!')
+  req.time = new Date().toLocaleTimeString()
+  next()
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
